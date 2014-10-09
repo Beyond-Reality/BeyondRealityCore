cfbbb03ce5ce21294b4b8eaefca66f1825921395
pauljoda <pauldavis101@gmail.com>
2014-10-08 20:17:28 -0500

    Main Menu Changes

---
+++ src/main/java/com/mcbeyondreality/beyondrealitycore/BRCGuiMainMenu.java
@@ -0,0 +1,426 @@
+package com.mcbeyondreality.beyondrealitycore;
+
+import com.google.common.base.Strings;
+import com.mcbeyondreality.beyondrealitycore.handlers.GuiMainMenuHandler;
+import cpw.mods.fml.client.GuiModList;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.*;
+import net.minecraft.client.renderer.OpenGlHelper;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.realms.RealmsBridge;
+import net.minecraft.util.EnumChatFormatting;
+import net.minecraft.util.MathHelper;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.demo.DemoWorldServer;
+import net.minecraft.world.storage.ISaveFormat;
+import net.minecraft.world.storage.WorldInfo;
+import org.apache.commons.io.Charsets;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.GLContext;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.util.*;
+
+@SideOnly(Side.CLIENT)
+public class BRCGuiMainMenu extends GuiScreen implements GuiYesNoCallback
+{
+    public static final Logger logger = LogManager.getLogger();
+    /** The RNG used by the Main Menu Screen. */
+    public static final Random rand = new Random();
+    /** Counts the number of screen updates. */
+    public float updateCounter;
+    /** The splash message. */
+    public String splashText;
+    public GuiButton buttonResetDemo;
+    /** Timer used to rotate the panorama, increases every tick. */
+    public int panoramaTimer;
+    /** Texture allocated for the current viewport of the main menu's panorama background. */
+    public DynamicTexture viewportTexture;
+    public final Object field_104025_t = new Object();
+    public String field_92025_p;
+    public String field_146972_A;
+    public String field_104024_v;
+    public static final ResourceLocation splashTexts = new ResourceLocation("beyondrealitycore:texts/splashes.txt");
+    public static final ResourceLocation minecraftTitleTextures = new ResourceLocation("textures/gui/title/minecraft.png");
+    public static final ResourceLocation backGround = new ResourceLocation("beyondrealitycore:textures/background.png");
+    public static final String field_96138_a = "Please click " + EnumChatFormatting.UNDERLINE + "here" + EnumChatFormatting.RESET + " for more information.";
+    public int field_92024_r;
+    public int field_92023_s;
+    public int field_92022_t;
+    public int field_92021_u;
+    public int field_92020_v;
+    public int field_92019_w;
+    public ResourceLocation field_110351_G;
+    private static final String __OBFID = "CL_00001154";
+
+    public BRCGuiMainMenu()
+    {
+        this.field_146972_A = field_96138_a;
+        this.splashText = "Shrek is love, Shrek is life";
+        BufferedReader bufferedreader = null;
+
+        try
+        {
+            ArrayList arraylist = new ArrayList();
+            bufferedreader = new BufferedReader(new InputStreamReader(Minecraft.getMinecraft().getResourceManager().getResource(splashTexts).getInputStream(), Charsets.UTF_8));
+            String s;
+
+            while ((s = bufferedreader.readLine()) != null)
+            {
+                s = s.trim();
+
+                if (!s.isEmpty())
+                {
+                    arraylist.add(s);
+                }
+            }
+
+            if (!arraylist.isEmpty())
+            {
+                do
+                {
+                    this.splashText = (String)arraylist.get(rand.nextInt(arraylist.size()));
+                }
+                while (this.splashText.hashCode() == 125780783);
+            }
+        }
+        catch (IOException ioexception1)
+        {
+            ;
+        }
+        finally
+        {
+            if (bufferedreader != null)
+            {
+                try
+                {
+                    bufferedreader.close();
+                }
+                catch (IOException ioexception)
+                {
+                    ;
+                }
+            }
+        }
+
+        this.updateCounter = rand.nextFloat();
+        this.field_92025_p = "";
+
+        if (!GLContext.getCapabilities().OpenGL20 && !OpenGlHelper.func_153193_b())
+        {
+            this.field_92025_p = I18n.format("title.oldgl1", new Object[0]);
+            this.field_146972_A = I18n.format("title.oldgl2", new Object[0]);
+            this.field_104024_v = "https://help.mojang.com/customer/portal/articles/325948?ref=game";
+        }
+    }
+
+    /**
+     * Called from the main game loop to update the screen.
+     */
+    public void updateScreen()
+    {
+        ++this.panoramaTimer;
+    }
+
+    /**
+     * Returns true if this GUI should pause the game when it is displayed in single-player
+     */
+    public boolean doesGuiPauseGame()
+    {
+        return false;
+    }
+
+    /**
+     * Fired when a key is typed. This is the equivalent of KeyListener.keyTyped(KeyEvent e).
+     */
+    protected void keyTyped(char p_73869_1_, int p_73869_2_) {}
+
+    /**
+     * Adds the buttons (and other controls) to the screen in question.
+     */
+    public void initGui()
+    {
+        this.viewportTexture = new DynamicTexture(256, 256);
+        this.field_110351_G = this.mc.getTextureManager().getDynamicTextureLocation("background", this.viewportTexture);
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());
+
+        if (calendar.get(2) + 1 == 7 && calendar.get(5) == 28)
+        {
+            this.splashText = "Happy birthday, pauljoda!";
+        }
+        else if (calendar.get(2) + 1 == 6 && calendar.get(5) == 1)
+        {
+            this.splashText = "Happy birthday, Notch!";
+        }
+        else if (calendar.get(2) + 1 == 12 && calendar.get(5) == 24)
+        {
+            this.splashText = "Merry X-mas!";
+        }
+        else if (calendar.get(2) + 1 == 1 && calendar.get(5) == 1)
+        {
+            this.splashText = "Happy new year!";
+        }
+        else if (calendar.get(2) + 1 == 10 && calendar.get(5) == 31)
+        {
+            this.splashText = "OOoooOOOoooo! Spooky!";
+        }
+
+        boolean flag = true;
+        int i = this.height / 4 + 48;
+
+        if (this.mc.isDemo())
+        {
+            this.addDemoButtons(i, 24);
+        }
+        else
+        {
+            this.addSingleplayerMultiplayerButtons(i, 24);
+        }
+
+        this.buttonList.add(new GuiButton(0, this.width / 2 - 100, i + 72 + 12, 98, 20, I18n.format("menu.options", new Object[0])));
+        this.buttonList.add(new GuiButton(4, this.width / 2 + 2, i + 72 + 12, 98, 20, I18n.format("menu.quit", new Object[0])));
+        this.buttonList.add(new GuiButtonLanguage(5, this.width / 2 - 124, i + 72 + 12));
+        Object object = this.field_104025_t;
+
+        synchronized (this.field_104025_t)
+        {
+            this.field_92023_s = this.fontRendererObj.getStringWidth(this.field_92025_p);
+            this.field_92024_r = this.fontRendererObj.getStringWidth(this.field_146972_A);
+            int j = Math.max(this.field_92023_s, this.field_92024_r);
+            this.field_92022_t = (this.width - j) / 2;
+            this.field_92021_u = ((GuiButton)this.buttonList.get(0)).yPosition - 24;
+            this.field_92020_v = this.field_92022_t + j;
+            this.field_92019_w = this.field_92021_u + 24;
+        }
+    }
+
+    /**
+     * Adds Singleplayer and Multiplayer buttons on Main Menu for players who have bought the game.
+     */
+    private void addSingleplayerMultiplayerButtons(int p_73969_1_, int p_73969_2_)
+    {
+        this.buttonList.add(new GuiButton(1, this.width / 2 - 100, p_73969_1_, I18n.format("menu.singleplayer", new Object[0])));
+        this.buttonList.add(new GuiButton(2, this.width / 2 - 100, p_73969_1_ + p_73969_2_ * 1, I18n.format("menu.multiplayer", new Object[0])));
+        GuiButton realmsButton = new GuiButton(14, this.width / 2 - 100, p_73969_1_ + p_73969_2_ * 2, I18n.format("menu.online", new Object[0]));
+        GuiButton fmlModButton = new GuiButton(6, this.width / 2 - 100, p_73969_1_ + p_73969_2_ * 2, "Mods");
+        fmlModButton.xPosition = this.width / 2 + 2;
+        realmsButton.width = 98;
+        fmlModButton.width = 98;
+        this.buttonList.add(realmsButton);
+        this.buttonList.add(fmlModButton);
+    }
+
+    /**
+     * Adds Demo buttons on Main Menu for players who are playing Demo.
+     */
+    private void addDemoButtons(int p_73972_1_, int p_73972_2_)
+    {
+        this.buttonList.add(new GuiButton(11, this.width / 2 - 100, p_73972_1_, I18n.format("menu.playdemo", new Object[0])));
+        this.buttonList.add(this.buttonResetDemo = new GuiButton(12, this.width / 2 - 100, p_73972_1_ + p_73972_2_ * 1, I18n.format("menu.resetdemo", new Object[0])));
+        ISaveFormat isaveformat = this.mc.getSaveLoader();
+        WorldInfo worldinfo = isaveformat.getWorldInfo("Demo_World");
+
+        if (worldinfo == null)
+        {
+            this.buttonResetDemo.enabled = false;
+        }
+    }
+
+    protected void actionPerformed(GuiButton p_146284_1_)
+    {
+        if (p_146284_1_.id == 0)
+        {
+            this.mc.displayGuiScreen(new GuiOptions(this, this.mc.gameSettings));
+        }
+
+        if (p_146284_1_.id == 5)
+        {
+            this.mc.displayGuiScreen(new GuiLanguage(this, this.mc.gameSettings, this.mc.getLanguageManager()));
+        }
+
+        if (p_146284_1_.id == 1)
+        {
+            this.mc.displayGuiScreen(new GuiSelectWorld(this));
+        }
+
+        if (p_146284_1_.id == 2)
+        {
+            this.mc.displayGuiScreen(new GuiMultiplayer(this));
+        }
+
+        if (p_146284_1_.id == 14)
+        {
+            this.func_140005_i();
+        }
+
+        if (p_146284_1_.id == 4)
+        {
+            this.mc.shutdown();
+        }
+
+        if (p_146284_1_.id == 6)
+        {
+            this.mc.displayGuiScreen(new GuiModList(this));
+        }
+
+        if (p_146284_1_.id == 11)
+        {
+            this.mc.launchIntegratedServer("Demo_World", "Demo_World", DemoWorldServer.demoWorldSettings);
+        }
+
+        if (p_146284_1_.id == 12)
+        {
+            ISaveFormat isaveformat = this.mc.getSaveLoader();
+            WorldInfo worldinfo = isaveformat.getWorldInfo("Demo_World");
+
+            if (worldinfo != null)
+            {
+                GuiYesNo guiyesno = GuiSelectWorld.func_152129_a(this, worldinfo.getWorldName(), 12);
+                this.mc.displayGuiScreen(guiyesno);
+            }
+        }
+    }
+
+    private void func_140005_i()
+    {
+        RealmsBridge realmsbridge = new RealmsBridge();
+        realmsbridge.switchToRealms(this);
+    }
+
+    public void confirmClicked(boolean p_73878_1_, int p_73878_2_)
+    {
+        if (p_73878_1_ && p_73878_2_ == 12)
+        {
+            ISaveFormat isaveformat = this.mc.getSaveLoader();
+            isaveformat.flushCache();
+            isaveformat.deleteWorldDirectory("Demo_World");
+            this.mc.displayGuiScreen(this);
+        }
+        else if (p_73878_2_ == 13)
+        {
+            if (p_73878_1_)
+            {
+                try
+                {
+                    Class oclass = Class.forName("java.awt.Desktop");
+                    Object object = oclass.getMethod("getDesktop", new Class[0]).invoke((Object)null, new Object[0]);
+                    oclass.getMethod("browse", new Class[] {URI.class}).invoke(object, new Object[] {new URI(this.field_104024_v)});
+                }
+                catch (Throwable throwable)
+                {
+                    logger.error("Couldn\'t open link", throwable);
+                }
+            }
+
+            this.mc.displayGuiScreen(this);
+        }
+    }
+
+
+    /**
+     * Draws the screen and all the components in it.
+     */
+    public void drawScreen(int p_73863_1_, int p_73863_2_, float p_73863_3_)
+    {
+        GL11.glDisable(GL11.GL_ALPHA_TEST);
+        GL11.glEnable(GL11.GL_ALPHA_TEST);
+        Tessellator tessellator = Tessellator.instance;
+        this.mc.getTextureManager().bindTexture(backGround);
+        ScaledResolution scaledresolution = new ScaledResolution(mc, mc.displayWidth, mc.displayHeight);
+        double width = scaledresolution.getScaledWidth();
+        double height = scaledresolution.getScaledHeight();
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+        tessellator.startDrawingQuads();
+        tessellator.setColorOpaque_I(0x999999);
+        tessellator.addVertexWithUV(0, 0, 0, 0, 0);
+        tessellator.addVertexWithUV(0, height, 0, 0, 1);
+        tessellator.addVertexWithUV(width, height, 0, 1, 1);
+        tessellator.addVertexWithUV(width, 0, 0, 1, 0);
+        tessellator.draw();
+        short short1 = 274;
+        int k = this.width / 2 - short1 / 2;
+        byte b0 = 30;
+        this.mc.getTextureManager().bindTexture(minecraftTitleTextures);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+
+        if ((double)this.updateCounter < 1.0E-4D)
+        {
+            this.drawTexturedModalRect(k + 0, b0 + 0, 0, 0, 99, 44);
+            this.drawTexturedModalRect(k + 99, b0 + 0, 129, 0, 27, 44);
+            this.drawTexturedModalRect(k + 99 + 26, b0 + 0, 126, 0, 3, 44);
+            this.drawTexturedModalRect(k + 99 + 26 + 3, b0 + 0, 99, 0, 26, 44);
+            this.drawTexturedModalRect(k + 155, b0 + 0, 0, 45, 155, 44);
+        }
+        else
+        {
+            this.drawTexturedModalRect(k + 0, b0 + 0, 0, 0, 155, 44);
+            this.drawTexturedModalRect(k + 155, b0 + 0, 0, 45, 155, 44);
+        }
+
+        tessellator.setColorOpaque_I(-1);
+        GL11.glPushMatrix();
+        GL11.glTranslatef((float)(this.width / 2 + 90), 70.0F, 0.0F);
+        GL11.glRotatef(-20.0F, 0.0F, 0.0F, 1.0F);
+        float f1 = 1.8F - MathHelper.abs(MathHelper.sin((float)(Minecraft.getSystemTime() % 1000L) / 1000.0F * (float)Math.PI * 2.0F) * 0.1F);
+        f1 = f1 * 100.0F / (float)(this.fontRendererObj.getStringWidth(this.splashText) + 32);
+        GL11.glScalef(f1, f1, f1);
+        this.drawCenteredString(this.fontRendererObj, this.splashText, 0, -8, -256);
+        GL11.glPopMatrix();
+        String s = "Minecraft 1.7.10";
+
+        if (this.mc.isDemo())
+        {
+            s = s + " Demo";
+        }
+
+        List<String> brandings = GuiMainMenuHandler.getBrandings();
+        for (int i = 0; i < brandings.size(); i++)
+        {
+            String brd = brandings.get(i);
+            if (!Strings.isNullOrEmpty(brd))
+            {
+                this.drawString(this.fontRendererObj, brd, 2, this.height - ( 10 + i * (this.fontRendererObj.FONT_HEIGHT + 1)), 16777215);
+            }
+        }
+        String s1 = "Copyright Mojang AB. Do not distribute!";
+        this.drawString(this.fontRendererObj, s1, this.width - this.fontRendererObj.getStringWidth(s1) - 2, this.height - 10, -1);
+
+        if (this.field_92025_p != null && this.field_92025_p.length() > 0)
+        {
+            drawRect(this.field_92022_t - 2, this.field_92021_u - 2, this.field_92020_v + 2, this.field_92019_w - 1, 1428160512);
+            this.drawString(this.fontRendererObj, this.field_92025_p, this.field_92022_t, this.field_92021_u, -1);
+            this.drawString(this.fontRendererObj, this.field_146972_A, (this.width - this.field_92024_r) / 2, ((GuiButton)this.buttonList.get(0)).yPosition - 12, -1);
+        }
+
+        super.drawScreen(p_73863_1_, p_73863_2_, p_73863_3_);
+    }
+
+    /**
+     * Called when the mouse is clicked.
+     */
+    protected void mouseClicked(int p_73864_1_, int p_73864_2_, int p_73864_3_)
+    {
+        super.mouseClicked(p_73864_1_, p_73864_2_, p_73864_3_);
+        Object object = this.field_104025_t;
+
+        synchronized (this.field_104025_t)
+        {
+            if (this.field_92025_p.length() > 0 && p_73864_1_ >= this.field_92022_t && p_73864_1_ <= this.field_92020_v && p_73864_2_ >= this.field_92021_u && p_73864_2_ <= this.field_92019_w)
+            {
+                GuiConfirmOpenLink guiconfirmopenlink = new GuiConfirmOpenLink(this, this.field_104024_v, 13, true);
+                guiconfirmopenlink.func_146358_g();
+                this.mc.displayGuiScreen(guiconfirmopenlink);
+            }
+        }
+    }
+}
