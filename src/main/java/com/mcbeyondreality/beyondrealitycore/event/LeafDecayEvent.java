748953e3879fe2a57ef68587aa8067ebdecef455
pauljoda <pauldavis101@gmail.com>
2014-10-12 14:32:12 -0500

    Fixed for anything breaking logs

--- src/main/java/com/mcbeyondreality/beyondrealitycore/event/LeafDecayEvent.java
+++ src/main/java/com/mcbeyondreality/beyondrealitycore/event/LeafDecayEvent.java
@@ -3,7 +3,6 @@ package com.mcbeyondreality.beyondrealitycore.event;
 import com.mcbeyondreality.beyondrealitycore.BeyondRealityCore;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
 import net.minecraft.block.BlockLog;
-import net.minecraft.item.ItemAxe;
 import net.minecraftforge.event.world.BlockEvent;
 
 import java.util.Random;
@@ -16,17 +15,14 @@ public class LeafDecayEvent {
 
         if(event.block instanceof BlockLog)
         {
-            if(event.getPlayer().inventory.getCurrentItem().getItem() instanceof ItemAxe)
+            Random r = new Random();
+            for(int i = -3; i < 3; i++)
             {
-                Random r = new Random();
-                for(int i = -3; i < 3; i++)
+                for(int j = -3; j < 3; j++)
                 {
-                    for(int j = -3; j < 3; j++)
+                    for(int k = 0-3; k < 3; k++)
                     {
-                        for(int k = 0-3; k < 3; k++)
-                        {
-                            event.world.scheduleBlockUpdate(event.x + i, event.y + j, event.z + k, event.world.getBlock(event.x + i, event.y + j, event.z + k), r.nextInt(7) + 4);
-                        }
+                        event.world.scheduleBlockUpdate(event.x + i, event.y + j, event.z + k, event.world.getBlock(event.x + i, event.y + j, event.z + k), r.nextInt(7) + 4);
                     }
                 }
             }
