217bbcaf52ddddc4992c20f9a5f9400b89d1f5da
pauljoda <pauldavis101@gmail.com>
2014-10-27 23:43:53 -0500

    Started work on IGW

--- src/main/java/com/mcbeyondreality/beyondrealitycore/IGW/BeyondRealityTab.java
+++ src/main/java/com/mcbeyondreality/beyondrealitycore/IGW/BeyondRealityTab.java
@@ -1,25 +1,21 @@
 package com.mcbeyondreality.beyondrealitycore.IGW;
 
 import igwmod.gui.GuiWiki;
-import igwmod.gui.IPageLink;
-import igwmod.gui.IReservedSpace;
-import igwmod.gui.tabs.IWikiTab;
+import igwmod.gui.tabs.BaseWikiTab;
 import net.minecraft.client.renderer.entity.RenderItem;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.init.Blocks;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
-import org.lwjgl.opengl.GL11;
 
-import java.util.List;
-
-public class BeyondRealityTab implements IWikiTab{
+public class BeyondRealityTab extends BaseWikiTab {
     private static RenderItem itemRenderer;
     private static ItemStack drawingStack;
 
-    static {
+    public BeyondRealityTab() {
         itemRenderer = new RenderItem();
         itemRenderer.setRenderManager(RenderManager.instance);
+        pageEntries.add("changelog");
     }
 
     @Override
@@ -33,55 +29,12 @@ public class BeyondRealityTab implements IWikiTab{
     }
 
     @Override
-    public List<IReservedSpace> getReservedSpaces(){
-        return null;
+    protected String getPageName(String s) {
+        return I18n.format("tab.entry." + s);
     }
 
     @Override
-    public List<IPageLink> getPages(int[] indexes){
-        return null;
-    }
-
-    @Override
-    public int pagesPerTab(){
-        return 16;
-    }
-
-    @Override
-    public int pagesPerScroll(){
-        return 2;
-    }
-
-    @Override
-    public int getSearchBarAndScrollStartY(){
-        return 61;
-    }
-
-    @Override
-    public void renderForeground(GuiWiki gui, int mouseX, int mouseY){
-        if(drawingStack != null) {
-            if(drawingStack.getItem() instanceof ItemBlock) {
-                gui.renderRotatingBlockIntoGUI(gui, drawingStack, 55, 33, 2.8F);
-            } else {
-                GL11.glPushMatrix();
-                GL11.glTranslated(49, 20, 0);
-                GL11.glScaled(2.2, 2.2, 2.2);
-                itemRenderer.renderItemAndEffectIntoGUI(gui.getFontRenderer(), gui.mc.getTextureManager(), drawingStack, 0, 0);
-                GL11.glPopMatrix();
-            }
-        }
-    }
-
-    @Override
-    public void renderBackground(GuiWiki gui, int mouseX, int mouseY){}
-
-    @Override
-    public void onMouseClick(GuiWiki gui, int mouseX, int mouseY, int mouseKey){}
-
-    @Override
-    public void onPageChange(GuiWiki gui, String pageName, Object... metadata){
-        if(metadata.length > 0 && metadata[0] instanceof ItemStack) {
-            drawingStack = (ItemStack)metadata[0];
-        }
+    protected String getPageLocation(String s) {
+        return s;
     }
 }
