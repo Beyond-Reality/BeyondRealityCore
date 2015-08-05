ce6361e215ef5b2fdab66877c0101089e22075f1
Paul Davis <pauldavis101@gmail.com>
2015-08-05 15:52:08 -0500

    Update BlockChair.java

--- src/main/java/com/mcbeyondreality/beyondrealitycore/blocks/BlockChair.java
+++ src/main/java/com/mcbeyondreality/beyondrealitycore/blocks/BlockChair.java
@@ -37,6 +37,7 @@ public class BlockChair extends BlockContainer {
     @Override
     public boolean onBlockActivated(World world, int x, int y, int z, EntityPlayer player, int par6, float par7, float par8, float par9) {
         super.onBlockActivated(world, x, y, z, player, par6, par7, par8, par9);
+        setOnBlock(world, x, y, z, player, 9 * 0.0625);
         return sitOnBlock(world, x, y, z, player, 9 * 0.0625);
     }
 
