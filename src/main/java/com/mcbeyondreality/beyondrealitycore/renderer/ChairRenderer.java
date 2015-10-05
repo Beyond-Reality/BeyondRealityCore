0b56b77136ee8694055afd840c87a23a00df23dd
Dominance <josephthomasmoore@live.com>
2015-10-04 23:51:27 -0400

    Attempt to Fix chair render leaked flag

--- src/main/java/com/mcbeyondreality/beyondrealitycore/renderer/ChairRenderer.java
+++ src/main/java/com/mcbeyondreality/beyondrealitycore/renderer/ChairRenderer.java
@@ -22,14 +22,14 @@ public class ChairRenderer extends TileEntitySpecialRenderer {
 
         GL11.glTranslatef((float) x + 0.5F, (float) y, (float) z + 0.5F);
         GL11.glScaled(0.6, 0.6, 0.6);
-        GL11.glDisable(GL11.GL_BLEND);
+   //     GL11.glDisable(GL11.GL_BLEND);
         GL11.glDisable(GL11.GL_CULL_FACE);
 
         this.bindTexture(texture);
 
         this.model.render();
 
-        GL11.glEnable(GL11.GL_BLEND);
+   //    GL11.glEnable(GL11.GL_BLEND);
         GL11.glEnable(GL11.GL_CULL_FACE);
         GL11.glPopMatrix();
     }
