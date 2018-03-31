d64262a1853ca2068b2366cfb3c27567e5fd3663
Léa Gris <lea.gris@noiraude.net>
2018-03-31 21:28:56 +0200

    feat(model): 2blocks heigh pedestal
    Provides new double height model for pedestal
    Expand collision and bounding boxes accordingly
    Enable pedestal emitting light
    Fix rendering of pedestal item
    Only use vanilla textures references

--- src/main/java/com/beyondrealitygaming/core/block/BRPedestal.java
+++ src/main/java/com/beyondrealitygaming/core/block/BRPedestal.java
@@ -2,13 +2,46 @@ package com.beyondrealitygaming.core.block;
 
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.Entity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+import javax.annotation.Nullable;
+import java.util.List;
 
 public class BRPedestal extends BRUnbreakeableBlock {
 
+    private static final AxisAlignedBB bounds = new AxisAlignedBB(0.0d, 0.0d, 0.0d, 1.0d, 2.0d,1.0d);
+
     public BRPedestal(String name, CreativeTabs tabs) {
         super(name, tabs);
-        this.setLightValue(1.0F);
-        this.setBlockBoubds(0.0F, 0.0F, 0.0F, 1.0F, 2.0F, 1.0F);
+    }
+
+    @Override
+    @Deprecated
+    public AxisAlignedBB getBoundingBox(IBlockState state, IBlockAccess access, BlockPos pos) {
+        return bounds;
+    }
+
+    @Deprecated
+    @SideOnly(Side.CLIENT)
+    public AxisAlignedBB getSelectedBoundingBox(IBlockState state) {
+        return bounds;
+    }
+
+    @Override
+    @Deprecated
+    public void addCollisionBoxToList(IBlockState state, World worldIn, BlockPos pos, AxisAlignedBB entityBox, List<AxisAlignedBB> collidingBoxes, Entity entityIn, boolean somebool) {
+        addCollisionBoxToList(pos, entityBox, collidingBoxes, bounds);
+    }
+
+    @Override
+    public int getLightValue(IBlockState state, IBlockAccess access, BlockPos blockpos) {
+        return 15;
     }
 
     @Override
